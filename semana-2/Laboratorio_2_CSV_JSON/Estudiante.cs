74c735a28d1cfbd9cfa5124fb9555561f477a9de
Pablo Molina <202978209+pdmolinas@users.noreply.github.com>
2026-07-20 23:45:59 -0600

    Laboratorio 2

---
+++ semana-2/Laboratorio_2_CSV_JSON/Estudiante.cs
@@ -0,0 +1,5 @@
+public class Estudiante {
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Carrera { get; set; }
+}
---
+++ semana-2/Laboratorio_2_CSV_JSON/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main()
+    {
+        List<Estudiante> estudiantes = new List<Estudiante>();
+
+        string[] lineas = File.ReadAllLines("estudiantes.csv");
+
+        for (int i = 1; i < lineas.Length; i++)
+        {
+            string[] campos = lineas[i].Split(',');
+
+            Estudiante estudiante = new Estudiante
+            {
+                Id = int.Parse(campos[0]),
+                Nombre = campos[1],
+                Carrera = campos[2]
+            };
+
+            estudiantes.Add(estudiante);
+        }
+
+        foreach (Estudiante e in estudiantes)
+        {
+            Console.WriteLine($"{e.Id}. - {e.Nombre} - {e.Carrera}");
+        }
+
+        var opciones = new JsonSerializerOptions { WriteIndented = true };
+        string json = JsonSerializer.Serialize(estudiantes, opciones);
+        File.WriteAllText("estudiantes.json", json);
+
+        Console.WriteLine("\nArchivo estudiantes.json creado correctamente.");
+    }
+}
